50d085f805586b1afaf4895fcaf8f890be221a4b
Oskar van Velden <oskar@rakso.nl>
2022-08-13 17:30:02 +0200

    Setup floor generation 0.1 

--- entities/floor.gd
+++ entities/floor.gd
@@ -1,23 +1,11 @@
 extends Node2D
 
-export var speed := 10
+export var speed := 20
 
-onready var tile1 = $Tile1;
-onready var tile2 = $Tile2;
+onready var tile_manager = $TileManager;
 
 var TILE_WIDTH := 160;
 var START_X_POS := 80;
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
 func _process(delta: float) -> void:
-	if (tile1.position.x < START_X_POS - TILE_WIDTH / 2):
-		tile1.position.x = tile2.position.x + TILE_WIDTH / 2;
-	if (tile2.position.x < START_X_POS - TILE_WIDTH / 2):
-		tile2.position.x = tile1.position.x + TILE_WIDTH / 2;
-		
-	tile1.position.x -= delta * speed;
-	tile2.position.x -= delta * speed;
+	tile_manager.position.x -= delta * speed;
